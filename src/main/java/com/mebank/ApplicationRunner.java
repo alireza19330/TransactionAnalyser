7fcfdb6b4e918f891222e41a4e7f67e6221ce0f8
Alireza Abazari <Alireza.Abazari@unico.com.au>
2019-09-22 20:22:34 +1000

    refactor runner

--- src/main/java/com/mebank/ApplicationRunner.java
+++ src/main/java/com/mebank/ApplicationRunner.java
@@ -8,8 +8,14 @@ public class ApplicationRunner {
     public static void main(String[] args) {
 
         RelativeBalanceService relativeBalanceService = new RelativeBalanceService();
-        RelativeBalanceResponse balance = relativeBalanceService.getBalance("/Users/aabazari/Downloads/mine/TransactionsAnalyser/src/main/resources/input.csv", "20/10/2018 12:00:00", "20/10/2018 19:00:00", "ACC334455");
-        System.out.println("Relative balance for the period is: " + balance.getBalance() + "\nNumber of transactions included is: " + balance.getNumOfRecords());
+        String filePath = "/Users/aabazari/Downloads/mine/TransactionsAnalyser/src/main/resources/input.csv";
+        String fromDateString = "20/10/2018 12:00:00";
+        String toDateString = "20/10/2018 19:00:00";
+        String account = "ACC334455";
+
+        RelativeBalanceResponse balance = relativeBalanceService.getBalance(filePath, fromDateString, toDateString, account);
+        String template = "Relative balance for the period is: %s\nNumber of transactions included is: %s";
+        System.out.println(String.format(template, balance.getBalance(), balance.getNumOfRecords()));
 
     }
 }
